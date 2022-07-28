c012423b681165dd41eb38ad258825d3ecd2f938
Aleyna Aktaş <aleynaaktas627@gmail.com>
2022-07-28 17:57:24 +0300

    Add react router library & route for all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
-
+import Navbar from "./components/UI/organisms/Navbar/Navbar";
+import HomePage from "./components/UI/pages/HomePage/HomePage";
+import FriendsPage from "./components/UI/pages/FriendsPage/FriendsPage";
+import GamePage from "./components/UI/pages/GamePage/GamePage";
+import ProfilePage from "./components/UI/pages/ProfilePage/ProfilePage";
+import RoomPage from "./components/UI/pages/RoomPage/RoomPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="friends" element={<FriendsPage />} />
+          <Route path="game/:roomId" element={<GamePage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="rooms" element={<RoomPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- client/src/components/UI/organisms/Navbar/Navbar.js
+++ client/src/components/UI/organisms/Navbar/Navbar.js
@@ -20,45 +20,47 @@ const Navbar = () => {
   const { isOpenState, componentName } = isOpen;
 
   return (
-    <div style={styles.navContainer}>
-      <div style={styles.navText}>
-        <Text text="WORD RACE" color="white" fontSize="2.5rem" font="InterSemiBold" letterSpacing="0.2rem" />
+    <>
+      <div style={styles.navContainer}>
+        <div style={styles.navText}>
+          <Text text="WORD RACE" color="white" fontSize="2.5rem" font="InterSemiBold" letterSpacing="0.2rem" />
+        </div>
+        {(isOpenState === true) & (componentName === "RegisterModal") ? (
+          <RegisterModal setIsOpen={setIsOpen} isOpen={isOpen} modalClose={() => setIsOpen(false)} />
+        ) : (isOpenState === true) & (componentName === "LoginModal") ? (
+          <LoginModal setIsOpen={setIsOpen} isOpen={isOpen} modalClose={() => setIsOpen(false)} />
+        ) : (isOpenState === true) & (componentName === "ForgotPasswordModal") ? (
+          <ForgotPasswordModal setIsOpen={setIsOpen} isOpen={isOpen} modalClose={() => setIsOpen(false)} />
+        ) : null}
+        <div style={styles.navButton}>
+          <Button
+            onClick={() => onClick("RegisterModal")}
+            className="buttonHoverGold"
+            width="12rem"
+            height="4rem"
+            borderRadius="1.8rem"
+            margin="0 2rem"
+            buttonColor="#EBD894"
+            text="Sign Up"
+            textColor="#6B5814"
+            font="RobotoMedium"
+            fontSize="1.8rem"
+          />
+          <Button
+            onClick={() => onClick("LoginModal")}
+            className="buttonHoverYellow"
+            width="12rem"
+            height="4rem"
+            borderRadius="1.8rem"
+            buttonColor="#F7F0D7"
+            text="Login"
+            textColor="#6B5814"
+            font="RobotoMedium"
+            fontSize="1.8rem"
+          />
+        </div>
       </div>
-      {(isOpenState === true) & (componentName === "RegisterModal") ? (
-        <RegisterModal setIsOpen={setIsOpen} isOpen={isOpen} modalClose={() => setIsOpen(false)} />
-      ) : (isOpenState === true) & (componentName === "LoginModal") ? (
-        <LoginModal setIsOpen={setIsOpen} isOpen={isOpen} modalClose={() => setIsOpen(false)} />
-      ) : (isOpenState === true) & (componentName === "ForgotPasswordModal") ? (
-        <ForgotPasswordModal setIsOpen={setIsOpen} isOpen={isOpen} modalClose={() => setIsOpen(false)} />
-      ) : null}
-      <div style={styles.navButton}>
-        <Button
-          onClick={() => onClick("RegisterModal")}
-          className="buttonHoverGold"
-          width="12rem"
-          height="4rem"
-          borderRadius="1.8rem"
-          margin="0 2rem"
-          buttonColor="#EBD894"
-          text="Sign Up"
-          textColor="#6B5814"
-          font="RobotoMedium"
-          fontSize="1.8rem"
-        />
-        <Button
-          onClick={() => onClick("LoginModal")}
-          className="buttonHoverYellow"
-          width="12rem"
-          height="4rem"
-          borderRadius="1.8rem"
-          buttonColor="#F7F0D7"
-          text="Login"
-          textColor="#6B5814"
-          font="RobotoMedium"
-          fontSize="1.8rem"
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
